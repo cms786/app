1c2115872d39eced852c527d2d2832f3817a6115
cms786 <waqar.yns786@protonmail.com>
2024-06-26 09:46:13 +0500

    Update aap.js

--- aap.js
+++ aap.js
@@ -1,1 +1,9 @@
-alert (" Script working normal");
+function displayInfo(){
+			alert("This is an alert message box.");  // display string message
+
+			alert('This is a numer: ' + 100); // display result of a concatenation
+
+			alert(100); // display number
+
+			alert(Date()); // display current date
+		}
